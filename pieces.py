73b394d53f0493f304687146ce7ecb2e31b7ede6
MoMoFoShow <morgantf2000@gmail.com>
2021-01-31 11:13:44 -0800

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -1,9 +1,6 @@
-turn = 0  # even numbers represent white, odd numbers represent black
-gamestate = [] # when variable is used add global if not working
+from helper import check
 
-def dump():
-    for piece in gamestate:
-        print(piece)
+gamestate = [] # when variable is used add global if not working
 
 class Piece:
     def __init__(self, color, xpos, ypos):
@@ -33,28 +30,33 @@ class Piece:
                         # decimal isn't inputted into there to be able to tell user their mistake
                         return True
 
-    def nocheck(self): # validates that intended move will not put player in check
-        return True
+    def nocheck(self): # validates that intended move will not put player in check - uses check func from helper, on altered position where move has already been made.
+        '''intendedState = gamestate
+        # make the move in intendedState
+        if check(intendedState):
+            return False
+        else:
+            return True'''
+        pass
 
     def validate(self, x, y):  # validates move path specific to piece
-        # if self.opensquare(): redundant with for loop
         if self.inmap(x, y):
             if self.nocheck():
                 return True
 
     def move(self, x, y):
         if self.validate(x, y):
+            for piece in gamestate:
+                if (piece.xpos, piece.ypos) == (x, y):
+                    del piece
+                    break
             self.xpos = x
             self.ypos = y
             return True
         else:
             return False
 
-
 class Bishop(Piece):
-    #def __init__(self, color, xpos, ypos):
-        #super.__init__(color, xpos, ypos)
-
     def __str__(self):
         superstr = super().__str__()
         output = 'bishop \n'
@@ -63,27 +65,24 @@ class Bishop(Piece):
 
     def validate(self, x, y):
         if super().validate(x, y):
-            dx = x - super().xpos
-            dy = y - super().ypos
+            dx = x - self.xpos
+            dy = y - self.ypos
             pdx = abs(dx)
             pdy = abs(dy)
             if pdx != pdy:
                 return False
             for squares in range(pdx):
-                nxtx = super().xpos + (dx / pdx) * (squares + 1)
-                nxty = super().ypos + (dy / pdy) * (squares + 1)
-                squarestate = super().opensquare(nxtx, nxty)
+                nxtx = self.xpos + (dx / pdx) * (squares + 1)
+                nxty = self.ypos + (dy / pdy) * (squares + 1)
+                squarestate = self.opensquare(nxtx, nxty)
                 if not squarestate:
                     if nxtx != x:
                         return False
-                    if self.color != squarestate:
-                        return True
-
+                    if self.color == squarestate:
+                        return False
+        return True
 
 class Rook(Piece):
-    #def __init__(self, color, xpos, ypos):
-        #super.__init__(color, xpos, ypos)
-
     def __str__(self):
         superstr = super().__str__()
         output = 'rook \n'
@@ -96,22 +95,23 @@ class Rook(Piece):
             dy = y - self.ypos
             if dx != 0 and dy == 0:
                 for squares in range(dx):
-                    nxtx = super().xpos + (dx / abs(dx)) * (squares + 1)
-                    squarestate = super().opensquare(nxtx, y)
+                    nxtx = self.xpos + (dx / abs(dx)) * (squares + 1)
+                    squarestate = self.opensquare(nxtx, y)
                     if not squarestate:
                         if nxtx != x:
                             return False
-                        if self.color != squarestate:
-                            return True  # Some common code here - at the end of coding optimize
-                    elif dx == 0 and dy != 0:
-                        for squares in range(dy):
-                            nxty = super().xpos + (dy / abs(dy)) * (squares + 1)
-                            squarestate = super().opensquare(x, nxty)
-                            if not squarestate:
-                                if nxty != y:
-                                    return False
-                                if self.color != squarestate:
-                                    return True
+                        if self.color == squarestate:
+                            return False  # Some common code here - at the end of coding optimize
+            elif dx == 0 and dy != 0:
+                for squares in range(dy):
+                    nxty = self.ypos + (dy / abs(dy)) * (squares + 1)
+                    squarestate = self.opensquare(x, nxty)
+                    if not squarestate:
+                        if nxty != y:
+                            return False
+                        if self.color == squarestate:
+                            return False
+        return True
 
 class Queen(Piece):
     def __str__(self):
@@ -121,40 +121,41 @@ class Queen(Piece):
         return output
 
     def validate(self, x, y):
-        if super.validate(x, y):
-            dx = x - super().xpos
-            dy = y - super().xpos
+        if super().validate(x, y):
+            dx = x - self.xpos
+            dy = y - self.xpos
             pdx = abs(dx)
             pdy = abs(dy)
             if dx != 0 and dy == 0:
                 for squares in range(dx):
-                    nxtx = super().xpos + (dx / abs(dx)) * (squares + 1)
-                    squarestate = super().opensquare(nxtx, y)
+                    nxtx = self.xpos + (dx / abs(dx)) * (squares + 1)
+                    squarestate = self.opensquare(nxtx, y)
                     if not squarestate:
                         if nxtx != x:
                             return False
-                        if self.color != squarestate:
-                            return True  # Some common code here - at the end of coding optimize
+                        if self.color == squarestate:
+                            return False  # Some common code here - at the end of coding optimize
             elif dx == 0 and dy != 0:
                 for squares in range(dy):
-                    nxty = super().xpos + (dy / abs(dy)) * (squares + 1)
-                    squarestate = super().opensquare(x, nxty)
+                    nxty = self.ypos + (dy / abs(dy)) * (squares + 1)
+                    squarestate = self.opensquare(x, nxty)
                     if not squarestate:
                         if nxty != y:
                             return False
-                        if self.color != squarestate:
-                            return True
+                        if self.color == squarestate:
+                            return False
             elif pdx != pdy:
                 return False
             for squares in range(pdx):
-                nxtx = super().xpos + (dx / pdx) * (squares + 1)
-                nxty = super().ypos + (dy / pdy) * (squares + 1)
-                squarestate = super().opensquare(nxtx, nxty)
+                nxtx = self.xpos + (dx / pdx) * (squares + 1)
+                nxty = self.ypos + (dy / pdy) * (squares + 1)
+                squarestate = self.opensquare(nxtx, nxty)
                 if not squarestate:
                     if nxtx != x:
                         return False
-                    if self.color != squarestate:
-                        return True
+                    if self.color == squarestate:
+                        return False
+        return True
 
 class Advisor(Piece):
     def __str__(self):
@@ -168,39 +169,79 @@ class Advisor(Piece):
             if (abs(King.xpos-x) > 1 or abs(King.ypos-y) > 1) and (abs(Queen.xpos-x) > 1 or abs(Queen.ypos-y) > 1):
                 # improve logic for how advisor moves after queen is dead
                 return False
-            dx = x - super().xpos
-            dy = y - super().xpos
+            dx = x - self.xpos
+            dy = y - self.xpos
             pdx = abs(dx)
             pdy = abs(dy)
             if dx != 0 and dy == 0:
                 for squares in range(dx):
-                    nxtx = super().xpos + (dx / abs(dx)) * (squares + 1)
-                    squarestate = super().opensquare(nxtx, y)
+                    nxtx = self.xpos + (dx / abs(dx)) * (squares + 1)
+                    squarestate = self.opensquare(nxtx, y)
                     if not squarestate:
                         if nxtx != x:
                             return False
-                        if self.color != squarestate:
-                            return True  # Some common code here - at the end of coding optimize
+                        if self.color == squarestate:
+                            return False  # Some common code here - at the end of coding optimize
             elif dx == 0 and dy != 0:
                 for squares in range(dy):
-                    nxty = super().xpos + (dy / abs(dy)) * (squares + 1)
-                    squarestate = super().opensquare(x, nxty)
+                    nxty = self.xpos + (dy / abs(dy)) * (squares + 1)
+                    squarestate = self.opensquare(x, nxty)
                     if not squarestate:
                         if nxty != y:
                             return False
-                        if self.color != squarestate:
-                            return True
+                        if self.color == squarestate:
+                            return False
             elif pdx != pdy:
                 return False
             for squares in range(pdx):
-                nxtx = super().xpos + (dx / pdx) * (squares + 1)
-                nxty = super().ypos + (dy / pdy) * (squares + 1)
-                squarestate = super().opensquare(nxtx, nxty)
+                nxtx = self.xpos + (dx / pdx) * (squares + 1)
+                nxty = self.ypos + (dy / pdy) * (squares + 1)
+                squarestate = self.opensquare(nxtx, nxty)
                 if not squarestate:
                     if nxtx != x:
                         return False
-                    if self.color != squarestate:
-                        return True
+                    if self.color == squarestate:
+                        return False
+        return True
+
+class Pawn(Piece):
+    def __str__(self):
+        superstr = super().__str__()
+        output = 'Pawn \n'
+        output = output + superstr
+        return output
+
+
+
+class King(Piece):
+    def __str__(self):
+        superstr = super().__str__()
+        output = 'King \n'
+        output = output + superstr
+        return output
+
+    def validate(self, x, y):
+        if super().validate(x, y):
+            if self.opensquare(x, y):
+                pdx = abs(x - self.xpos)
+                pdy = abs(y - self.xpos)
+                if (pdx == 1 and pdy == 1) or (pdx+pdy == 1):
+                    return True
+                else:
+                    return False
+
+
+
+
+class Knight(Piece):
+    def __str__(self):
+        superstr = super().__str__()
+        output = 'Knight \n'
+        output = output + superstr
+        return output
+
+
+
 
 # initial piece positions below
 
@@ -216,6 +257,9 @@ gamestate = [Rook('white', 1, 1),
              Queen('black', 4, 8),
              Advisor('white', 6, 1),
              Advisor('black', 6, 8)]
+
+
+
 
 
 
